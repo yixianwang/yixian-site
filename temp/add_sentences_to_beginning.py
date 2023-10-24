fc0a1166a20a5dd1197b020b5d2c3804c8b2115d
yixianwang <wangyixian.wyx@gmail.com>
2023-10-24 03:39:17 -0400

    fix: add file name automatically to python script

--- temp/add_sentences_to_beginning.py
+++ temp/add_sentences_to_beginning.py
@@ -1,12 +1,5 @@
 import os
 
-# Define the sentence you want to add
-sentence_to_add = """+++
-title = 'Markdown'
-date = 2023-10-23T21:50:46-04:00
-draft = true
-+++
-\n"""
 
 # Specify the directory where your Markdown files are located
 markdown_directory = "./"
@@ -17,7 +10,14 @@ markdown_files = [f for f in os.listdir(markdown_directory) if f.endswith(".md")
 # Loop through each Markdown file and add the sentence
 for markdown_file in markdown_files:
     file_path = os.path.join(markdown_directory, markdown_file)
+
+    # Extract the file name from the file path
+    file_name = os.path.basename(file_path)[:-3]
     
+
+    # Define the sentence you want to add
+    sentence_to_add = f"""+++\ntitle = '{file_name}'\ndate = 2023-10-24T03:10:46-04:00\ndraft = true\n+++\n\n"""
+
     # Open the file in read mode to read its content
     with open(file_path, 'r') as file:
         content = file.read()
